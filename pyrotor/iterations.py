2827fad3df5818995e13058923867535cbded4eb
ArthurTlprt <arthur.talpaert@inria.fr>
2020-07-28 10:04:54 +0200

    commit to pull

--- pyrotor/iterations.py
+++ pyrotor/iterations.py
@@ -41,32 +41,31 @@ def compute_g(x, Q, W):
     return a + b
 
 
-def binary_search_best_trajectory(self, i, step):
-    self.i_kappa = i
-    self.i_binary_search += 1
-    print("Kappa #"+str(i))
+def binary_search_best_trajectory(trajectory, i, step):
+    trajectory.i_kappa = i
+    trajectory.i_binary_search += 1
     if i < 0:
-        raise ValueError("Flights of reference too close to VMO or MMO:\nAborted")
-    self.weight_dict = {k: self.kappas[i] * self.original_weights[k]
-                        for k in self.original_weights}
-    self.compute_trajectory()
+        raise ValueError("Trajectories of reference too close to your constraints:\nAborted")
+    trajectory.weight_dict = {k: trajectory.kappas[i] * trajectory.original_weights[k]
+                              for k in trajectory.original_weights}
+    trajectory.compute_trajectory()
 
     step = step//2
-    if not self.is_valid:
+    if not trajectory.is_valid:
         if step == 0:
             step = 1
-        self.binary_search_best_opti(i-step, step)
+        trajectory.binary_search_best_opti(i-step, step)
     else:
-        if len(self.kappas)-1 != i and step != 0:
-            self.binary_search_best_opti(i+step, step)
+        if len(trajectory.kappas)-1 != i and step != 0:
+            trajectory.binary_search_best_opti(i+step, step)
 
 
-def compute_trajectory_kappa(self):
+def compute_trajectory_kappa(trajectory):
     from copy import copy
-    self.original_weights = copy(self.weight_dict)
+    trajectory.original_weights = copy(trajectory.weight_dict)
     x = np.linspace(0, 1, 30)
-    self.kappas = 1/np.exp(5*x**(1/2))
-    self.i_binary_search = 0
-    self.binary_search_best_opti(len(self.kappas)-1, len(self.kappas)-1)
+    trajectory.kappas = 1/np.exp(5*x**(1/2))
+    trajectory.i_binary_search = 0
+    trajectory.binary_search_best_opti(len(trajectory.kappas)-1, len(trajectory.kappas)-1)
     if not self.is_valid:
-        raise ValueError("Flights of reference too close to VMO or MMO:\nAborted")
+        raise ValueError("Trajectories of reference too close to your constraints:\nAborted")
